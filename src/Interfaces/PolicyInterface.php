78ac7117038d51f2c517c78eec87ce6c5abbd01c
Michael Lambert <michael@alphageek.com.au>
2020-04-06 07:57:31 +1000

    :sparkles: Add Policy interface

---
+++ src/Interfaces/PolicyInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace NibyNool\PasswordPolicy\Interfaces;
+
+use NibyNool\PasswordPolicy\Exceptions\PasswordValidationException;
+use NibyNool\PasswordPolicy\Exceptions\PolicyConfigurationException;
+
+/**
+ * Policy Interface
+ *
+ * The basic requirements for creating a new policy that can be enforced on both frontend (JS) and backend (PHP)
+ */
+interface PolicyInterface
+{
+    /**
+     * Class constructor that accepts a configuration
+     *
+     * @param mixed $config
+     *
+     * @throws PolicyConfigurationException
+     */
+    public function __construct($config = null);
+
+    /**
+     * Get a string describing (in human readable terms) the password policy.
+     *
+     * @return string
+     */
+    public function getPolicyDescription();
+
+    /**
+     * Validate the supplied password against the policy.
+     *
+     * If the password is valid return true, otherwise throw a PasswordValidationExceptions
+     *
+     * @param string $password The password to be validated
+     *
+     * @return bool
+     * @throws PasswordValidationException
+     */
+    public function validatePassword($password);
+}
