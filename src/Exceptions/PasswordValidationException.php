e2625055b573c2c673967c0ca256d62835d28d02
Michael Lambert <michael@alphageek.com.au>
2020-04-06 07:57:12 +1000

    :sparkles: Add Exceptions

---
+++ src/Exceptions/PasswordValidationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NibyNool\PasswordPolicy\Exceptions;
+
+/**
+ * Password Validation Exception
+ *
+ * The exception to be thrown when a password does not conform to the policy
+ */
+class PasswordValidationException extends \UnexpectedValueException
+{
+
+}
---
+++ src/Exceptions/PolicyConfigurationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NibyNool\PasswordPolicy\Exceptions;
+
+/**
+ * Policy Configuration Exception
+ *
+ * The exception to be thrown when a policy configuration is invalid
+ */
+class PolicyConfigurationException extends \UnexpectedValueException
+{
+
+}
